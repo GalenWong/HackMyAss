c6452c324767f5d613d729b6fbe97046dcef658b
GalenWong <wonggalen1999@gmail.com>
2019-03-30 15:54:16 -0700

    XML replacement

--- inject.js
+++ inject.js
@@ -14,6 +14,25 @@ const matchAll = (userinfo, string) => {
     return result;
 };
 
+/**
+ * generateLeakReport will issue consent box.
+ * If user consent is not given, it will throw an Error.
+ * @param {Object} leaked contains leaked data to display
+ * @returns {undefined} nothing is returned
+ */
+const generateLeakReport = leaked => {
+    const leakedKeys = Object.keys(leaked);
+    if (leakedKeys.length === 0) return;
+    
+    let warning = 'The website tries to send the following data to server:\n';
+    for (let k of leakedKeys) {
+        warning += `${k}: ${leaked[k]}\n`;
+    }
+    warning += 'Give Permission?'
+    if (confirm(warning)) return; // given consent
+    throw "User consent not given";
+}
+
 const fetchCheck = (userinfo, ...args) => {
     const URL = args[0];
     const { headers, body } = args[1]?args[1]:{};
@@ -41,26 +60,39 @@ const fetchCheck = (userinfo, ...args) => {
         const leaked = matchAll(userinfo, marshalled);
         allLeaks = {...allLeaks, ...leaked};
     }
+    generateLeakReport(allLeaks);
+};
 
-    const leakedKeys = Object.keys(allLeaks);
-    if (leakedKeys.length === 0) return;
-    
-    let warning = 'The website tries to send the following data to server:\n';
-    for (let k of leakedKeys) {
-        warning += `${k}: ${allLeaks[k]}\n`;
+const XMLSendCheck = (userinfo, body) => {
+    let allLeaks = new Object;
+    if (isString(body)) {
+        const leaked = matchAll(userinfo, body);
+        allLeaks = { ...allLeaks, ...leaked };
+    } else if (body instanceof Object) {
+        const marshalled = JSON.stringify(body);
+        const leaked = matchAll(userinfo, marshalled);
+        allLeaks = { ...allLeaks, ...leaked };
     }
-    warning += 'Give Permission?'
-    if (confirm(warning)) return; // given consent
-    throw "User consent not given";
+    generateLeakReport(allLeaks);
+};
+
+const XMLOpenCheck = (userinfo, URL) => {
+    if(isString(URL)) {
+        const leaked = matchAll(userinfo, URL);
+        generateLeakReport(leaked);
+    }
+}
+
+// TODO: remove this
+const tempUserInfo = {
+    "username": "GalenWong",
 };
 
 function replaceFetch() {
     const oldfetch = window.fetch;
     const newfetch = async (...args) => {
-        fetchCheck({
-            "username": "GalenWong",
-        }, ...args);
-        return oldfetch(...args);
+        fetchCheck(tempUserInfo, ...args);
+        return oldfetch.call(this, ...args);
     }
 
     window.fetch = newfetch;
@@ -69,13 +101,28 @@ function replaceFetch() {
 }
 
 function replaceXML() {
-    const oldsend = window.XMLHttpRequest.send;
+    const oldsend = window.XMLHttpRequest.prototype.send;
+    async function newsend (body) {
+        XMLSendCheck(tempUserInfo, body);
+        return oldsend.call(this, body);
+    }
+    window.XMLHttpRequest.prototype.send = newsend;
+    console.log('XMLHttpRequest.send replaced')
+
+    const oldopen = window.XMLHttpRequest.prototype.open;
+    async function newopen (method, URL, ...args) {
+        XMLOpenCheck(tempUserInfo, URL);
+        return oldopen.call(this, method, URL, ...args);
+    }
+    window.XMLHttpRequest.prototype.open = newopen;
+    console.log('XMLHttpRequest.open replaced')
 }
 
 (function(){
     /* Script init check */
     console.log('HackMyAss is running');
     replaceFetch();
+    replaceXML();
 
 })();
 
